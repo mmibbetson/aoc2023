14af7740ab14a9f3bde266aaf781087f92a85197
Matthew Ibbetson <matthew.ibbetson@kcpivot.co.za>
2024-02-26 07:46:28 +0200

    port over existing repo from gitlab

---
+++ Trebuchet/src/Trebuchet/Utils.fs
@@ -0,0 +1,21 @@
+﻿module Trebuchet.Utils
+
+open System
+
+open System.IO
+
+open Trebuchet.Parser
+
+let input =
+    let args = Environment.GetCommandLineArgs() in
+
+    match args |> Seq.length with
+    | 2 -> File.ReadAllLines(args[1])
+    | _ -> ArgumentException("Please provide an input file") |> raise
+
+let deriveCalibrationNumbers line =
+    line
+    |> transformNumberWords
+    |> numbersOnly
+    |> firstAndLast
+    |> int
