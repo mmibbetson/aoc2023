0f9a3686b6cac0e29bd9432a8d9c036f02b0eda4
Matthew Ibbetson <mmibbetson@gmail.com>
2024-03-10 12:12:13 +0200

    begin getting adjacent positions funs

--- GearRatios/src/GearRatios/Utils.fs
+++ GearRatios/src/GearRatios/Utils.fs
@@ -14,9 +14,21 @@ let createSchematic (input: string array) =
 
                 { Coordinates = coordinates
                   Character = character }))
-        |> Seq.collect id
+        |> Seq.concat
 
     schematic
 
 let partNumbers (schematic: Position seq) =
+    schematic
+    |> Seq.fold (fun (state: int seq) position ->
+        match position.Character with
+        | '.' | '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' -> state
+        | _ -> position |> adjacentNumbers schematic |> Seq.append state) Seq.empty
     
+let adjacentCoordinates (schematic: Position seq) (coordinates: int * int) =
+    // get 8 adj coords
+    
+let adjacentNumbers (schematic: Position seq) (position: Position): int seq =
+    let adjacentPositions =
+        schematic
+        |> Seq.filter (fun pos -> adjacentCoordinates schematic position.Coordinates |> Seq.contains)
