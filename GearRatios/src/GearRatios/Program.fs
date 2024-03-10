8c30c3d95cd962e693505826723a6eae4fdfd741
Matthew Ibbetson <mmibbetson@gmail.com>
2024-03-10 23:54:53 +0200

    add working solution for part 1 (its yucky)

--- GearRatios/src/GearRatios/Program.fs
+++ GearRatios/src/GearRatios/Program.fs
@@ -1,13 +1,15 @@
 ﻿open System
 open System.IO
 
-open GearRatios.Types
+
 open GearRatios.Utils
 
-let input = Environment.GetCommandLineArgs()[1] |> File.ReadAllLines
 
+let input = Environment.GetCommandLineArgs()[1] |> File.ReadAllLines
 let schematic = createSchematic input
 
 let firstAnswer = schematic |> partNumbers |> Seq.sum
+let secondAnswer = schematic |> gearRatios |> Seq.sum
 
-printfn $"{firstAnswer}"
+printfn $"First Answer: {firstAnswer}"
+printfn $"Second Answer: {secondAnswer}"
--- GearRatios/src/GearRatios/Utils.fs
+++ GearRatios/src/GearRatios/Utils.fs
@@ -1,34 +1,82 @@
 ﻿module GearRatios.Utils
 
-open GearRatios.Types
 
-let createSchematic (input: string array) =
-    let asChars = input |> Seq.map _.ToCharArray()
+open System
 
-    let schematic =
-        asChars
-        |> Seq.mapi (fun row columns ->
-            columns
-            |> Seq.mapi (fun column character ->
-                let coordinates = (row, column) in
 
-                { Coordinates = coordinates
-                  Character = character }))
-        |> Seq.concat
+let createSchematic lines =
+    let generatePoints =
+        Seq.mapi (fun rowIdx row -> row |> Seq.mapi (fun colIdx character -> ((rowIdx, colIdx), character)))
 
+    lines |> generatePoints |> Seq.concat |> Map.ofSeq
+
+
+let adjacentCoordinates (row, column) =
+    [| (row - 1, column - 1)
+       (row - 1, column)
+       (row - 1, column + 1)
+       (row, column - 1)
+       (row, column + 1)
+       (row + 1, column - 1)
+       (row + 1, column)
+       (row + 1, column + 1) |]
+
+
+let rec scanLeft schematic (row, column) =
+    let leftCharacterKey = (row, column - 1) in
+
+    match Map.tryFind leftCharacterKey schematic with
+    | Some character ->
+        match Char.IsDigit character with
+        | true -> scanLeft schematic leftCharacterKey + string character
+        | false -> ""
+    | None -> ""
+
+
+let rec scanRight schematic (row, column) =
+    let rightCharacterKey = (row, column + 1) in
+
+    match Map.tryFind rightCharacterKey schematic with
+    | Some character ->
+        match Char.IsDigit character with
+        | true -> string character + scanRight schematic rightCharacterKey
+        | false -> ""
+    | None -> ""
+
+
+let adjacentNumbers schematic symbolPosition =
+    let adjacentNumberPositions =
+        symbolPosition
+        |> adjacentCoordinates
+        |> Seq.filter (fun adjacentPosition ->
+            match Map.tryFind adjacentPosition schematic with
+            | Some position -> Char.IsDigit position
+            | None -> false) in
+
+    adjacentNumberPositions
+    |> Seq.map (fun adjacentPosition ->
+        let leftPortion = scanLeft schematic adjacentPosition
+        let middlePortion = schematic.Item adjacentPosition |> string
+        let rightPortion = scanRight schematic adjacentPosition
+
+        leftPortion + middlePortion + rightPortion)
+    |> Seq.map int
+    |> Seq.distinct
+
+
+let partNumbers schematic =
     schematic
+    |> Map.filter (fun _ value -> not (Char.IsDigit value) && not (value = '.'))
+    |> Map.fold (fun acc key _ -> key |> adjacentNumbers schematic |> Seq.append acc) Seq.empty
 
-let partNumbers (schematic: Position seq) =
+let gearRatios schematic =
     schematic
-    |> Seq.fold (fun (state: int seq) position ->
-        match position.Character with
-        | '.' | '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' -> state
-        | _ -> position |> adjacentNumbers schematic |> Seq.append state) Seq.empty
-    
-let adjacentCoordinates (schematic: Position seq) (coordinates: int * int) =
-    // get 8 adj coords
-    
-let adjacentNumbers (schematic: Position seq) (position: Position): int seq =
-    let adjacentPositions =
-        schematic
-        |> Seq.filter (fun pos -> adjacentCoordinates schematic position.Coordinates |> Seq.contains)
+    |> Map.filter (fun key value -> value = '*' && (key |> adjacentNumbers schematic |> Seq.length) = 2)
+    |> Map.fold
+        (fun acc key _ ->
+            key
+            |> adjacentNumbers schematic
+            |> Seq.fold (*) 1
+            |> Seq.singleton
+            |> Seq.append acc)
+        Seq.empty
