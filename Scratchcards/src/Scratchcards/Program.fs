bbeba4b60b0538e15eba54e33cec88ba5fe43041
Matthew Ibbetson <mmibbetson@gmail.com>
2024-03-21 14:51:18 +0200

    release builds and final edits

--- Scratchcards/src/Scratchcards/Program.fs
+++ Scratchcards/src/Scratchcards/Program.fs
@@ -11,12 +11,12 @@ type GameCard =
 let constructGameCard (line: string) =
     let destructuredLine = line.Split(':', '|')
 
-    let getNumberArray (str: String) =
+    let toNumberArray (str: String) =
         str.Split(' ', StringSplitOptions.RemoveEmptyEntries) |> Array.map int
 
     let id = destructuredLine[0] |> String.filter Char.IsDigit |> int
-    let winningNumbers = destructuredLine[1] |> getNumberArray
-    let cardNumbers = destructuredLine[2] |> getNumberArray
+    let winningNumbers = destructuredLine[1] |> toNumberArray
+    let cardNumbers = destructuredLine[2] |> toNumberArray
 
     { Id = id
       WinningNumbers = winningNumbers
@@ -35,21 +35,18 @@ let numberOfMatches gameCard =
     |> Array.length
 
 
-let rec numberGenerated (arr: int array) idx =
+let rec cardsGenerated (arr: int array) idx =
     match arr[idx] with
     | 0 -> 1
-    | matches ->
-        1
-        + Array.reduce (fun acc offset -> acc + numberGenerated arr (idx + offset)) [| 0..matches |]
-//  | matches -> 1 + ([| for offset in 1..matches -> numberGenerated (idx + offset) arr |] |> Array.sum)
-//  ^ AI generated alternative equivalent using an array comprehension
+    | matches -> 1 + Array.reduce (fun acc offset -> acc + cardsGenerated arr (idx + offset)) [| 0..matches |]
+//  | matches -> 1 + ([| for offset in 1..matches -> numberGenerated arr (idx + offset) |] |> Array.sum)
 
 
 let gameCards = Environment.GetCommandLineArgs()[1] |> File.ReadAllLines |> Array.map constructGameCard
 let matchList = gameCards |> Array.map numberOfMatches
 
 let firstAnswer = gameCards |> Array.map cardScore |> Array.sum
-let secondAnswer = matchList |> Array.mapi (fun idx _ -> numberGenerated matchList idx) |> Array.sum
+let secondAnswer = matchList |> Array.mapi (fun idx _ -> cardsGenerated matchList idx) |> Array.sum
 
 printfn $"First Answer: %d{firstAnswer}"
 printfn $"Second Answer: %d{secondAnswer}"
