bfdd55bcfaaa7eb2a27e9972258bdf54357a366d
Matthew Ibbetson <mmibbetson@gmail.com>
2024-03-21 14:18:09 +0200

    add solution for both steps

--- Scratchcards/src/Scratchcards/Program.fs
+++ Scratchcards/src/Scratchcards/Program.fs
@@ -9,18 +9,14 @@ type GameCard =
 
 
 let constructGameCard (line: string) =
-    let destructuredLine = line.Split([| ':'; '|' |])
+    let destructuredLine = line.Split(':', '|')
+
+    let getNumberArray (str: String) =
+        str.Split(' ', StringSplitOptions.RemoveEmptyEntries) |> Array.map int
+
     let id = destructuredLine[0] |> String.filter Char.IsDigit |> int
-
-    let winningNumbers =
-        destructuredLine[1]
-        |> (_.Split(' ', StringSplitOptions.RemoveEmptyEntries))
-        |> Array.map int
-
-    let cardNumbers =
-        destructuredLine[2]
-        |> (_.Split(' ', StringSplitOptions.RemoveEmptyEntries))
-        |> Array.map int
+    let winningNumbers = destructuredLine[1] |> getNumberArray
+    let cardNumbers = destructuredLine[2] |> getNumberArray
 
     { Id = id
       WinningNumbers = winningNumbers
@@ -31,29 +27,29 @@ let cardScore gameCard =
     gameCard.CardNumbers
     |> Array.filter (fun num -> Array.contains num gameCard.WinningNumbers)
     |> Array.fold (fun acc _ -> if acc = 0 then 1 else acc * 2) 0
-    
+
+
 let numberOfMatches gameCard =
     gameCard.CardNumbers
     |> Array.filter (fun num -> Array.contains num gameCard.WinningNumbers)
     |> Array.length
 
-// take in an int array and for each elem keep track of how many clones they generate. need id's or smth
-let rec clone count 
 
-let input = Environment.GetCommandLineArgs()[1] |> File.ReadAllLines
+let rec numberGenerated (arr: int array) idx =
+    match arr[idx] with
+    | 0 -> 1
+    | matches ->
+        1
+        + Array.reduce (fun acc offset -> acc + numberGenerated arr (idx + offset)) [| 0..matches |]
+//  | matches -> 1 + ([| for offset in 1..matches -> numberGenerated (idx + offset) arr |] |> Array.sum)
+//  ^ AI generated alternative equivalent using an array comprehension
 
-input
-|> Array.map constructGameCard
-|> Array.map cardScore
-|> Array.sum
-|> fun n -> printfn $"First Answer: {n}"
 
-// get the game cards
-// evaluate how many matches each card has
-// apply those matches against the subsequent cards to mult their mults
-// sum the number of items in the end
+let gameCards = Environment.GetCommandLineArgs()[1] |> File.ReadAllLines |> Array.map constructGameCard
+let matchList = gameCards |> Array.map numberOfMatches
 
-input
-|> Array.map constructGameCard
-|> Array.map numberOfMatches
-|> 
+let firstAnswer = gameCards |> Array.map cardScore |> Array.sum
+let secondAnswer = matchList |> Array.mapi (fun idx _ -> numberGenerated matchList idx) |> Array.sum
+
+printfn $"First Answer: %d{firstAnswer}"
+printfn $"Second Answer: %d{secondAnswer}"
